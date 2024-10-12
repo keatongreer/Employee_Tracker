059409f3fb83d646a53220fe9da90ebaac7b78d3
Keaton Greer <keatongreer1@gmail.com>
2024-10-11 23:21:54 -0400

    Finished cli functionality

--- src/cli.ts
+++ src/cli.ts
@@ -64,7 +64,7 @@ class Cli {
                 //   console.table(result.rows);
                 console.log('\n');
                 printTable(result.rows);
-                const lineBuffer = Math.round(result.rowCount as number * 0.75);
+                const lineBuffer = Math.round(result.rowCount as number * 0.6);
                 for (let i = 0; i < lineBuffer; i++) {
                     console.log('\n');       
                 }
@@ -74,11 +74,167 @@ class Cli {
     }
 
     addEmployee(): void {
-        // TODO
+        // get array of available roles to give employee'
+        const roles: string[] = [];
+        pool.query('SELECT title FROM role', (err: Error, result: QueryResult) => {
+            if (err) {
+              console.log(err);
+            } else if (result) {
+                result.rows.forEach(role => {
+                    roles.push(role.title);
+                });
+            }
+        });
+
+        // get array of available employees to assign as manager'
+        const potentialManagers: string[] = [];
+        pool.query("SELECT first_name || ' ' || last_name AS name FROM employee", (err: Error, result: QueryResult) => {
+            if (err) {
+              console.log(err);
+            } else if (result) {
+                result.rows.forEach(potManager => {
+                    potentialManagers.push(potManager.name);
+                });
+            }
+        });
+
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'fname',
+                message: "What is the employee's first name?",
+            },
+            {
+                type: 'input',
+                name: 'lname',
+                message: "What is the employee's last name?",
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: "What is the employee's role?",
+                choices: roles,
+            },
+            {
+                type: 'list',
+                name: 'manager',
+                message: "Who is the employee's manager?",
+                choices: potentialManagers,
+            }
+        ])
+        .then((res) => {
+            // get role id by querying role.title
+            let roleId: number;
+            let managerId: number;
+
+            pool.query(`SELECT id FROM role WHERE title = $1`, [res.role], (err: Error, result: QueryResult) => {
+                if (err) {
+                    console.log(err);
+                } else if (result) {
+                    // get the role id from the result object
+                    roleId = result.rows[0].id;
+
+                    // get manager id by querying employees                    
+                    pool.query(`SELECT id FROM employee WHERE first_name || ' ' || last_name = $1`, [res.manager], (err: Error, result: QueryResult) => {
+                        if (err) {
+                            console.log(err);
+                        } else if (result) {
+                            managerId = result.rows[0].id;
+
+                            // insert the role into the correct department
+                            // perform this query inside of the other queries due to async response causing the ids to not be available otherwise
+                            pool.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`, [res.fname, res.lname, roleId, managerId], (err: Error, result: QueryResult) => {
+                                if (err) {
+                                    console.log(err);
+                                } else if (result) {
+                                    console.log(`Added ${res.fname} ${res.lname} to the database`);
+                                    this.startCli();
+                                }
+                            });
+                        }
+                    });        
+                }
+            });
+        });
     }
 
     updateEmployeeRole(): void {
-        // TODO
+        // get array of employees
+        const allEmployees: string[] = [];
+        const roles: string[] = [];
+
+        pool.query("SELECT first_name || ' ' || last_name AS name FROM employee", (err: Error, result: QueryResult) => {
+            if (err) {
+              console.log(err);
+            } else if (result) {
+                result.rows.forEach(employee => {
+                    allEmployees.push(employee.name);
+                });
+
+                // get array of available roles to give employee        
+                pool.query('SELECT title FROM role', (err: Error, result: QueryResult) => {
+                    if (err) {
+                    console.log(err);
+                    } else if (result) {
+                        result.rows.forEach(role => {
+                            roles.push(role.title);
+                        });
+
+                        inquirer
+                        .prompt([
+                            {
+                                type: 'list',
+                                name: 'employee',
+                                message: "Which employee's role do you want to update?",
+                                choices: allEmployees,
+                            },
+                            {
+                                type: 'list',
+                                name: 'role',
+                                message: "Which role do you want to assign the selected employee?",
+                                choices: roles,
+                            }
+                        ])
+                        .then((res) => {
+                            // get role id by querying role.title
+                            let employeeId: number;
+                            let roleId: number;
+
+                            pool.query(`SELECT id FROM role WHERE title = $1`, [res.role], (err: Error, result: QueryResult) => {
+                                if (err) {
+                                    console.log(err);
+                                } else if (result) {
+                                    // get the role id from the result object
+                                    roleId = result.rows[0].id;
+
+                                    // get employee id by querying employees                    
+                                    pool.query(`SELECT id FROM employee WHERE first_name || ' ' || last_name = $1`, [res.employee], (err: Error, result: QueryResult) => {
+                                        if (err) {
+                                            console.log(err);
+                                        } else if (result) {
+                                            employeeId = result.rows[0].id;
+
+                                            // update the employee's role
+                                            pool.query(`UPDATE employee SET role_id = $1 WHERE id = $2`, [roleId, employeeId], (err: Error, result: QueryResult) => {
+                                                if (err) {
+                                                    console.log(err);
+                                                } else if (result) {
+                                                    console.log(`Updated employee's role`);
+                                                    this.startCli();
+                                                }
+                                            });
+                                        }
+                                    });        
+                                }
+                            });
+                        });
+                    }
+                });
+            }
+        });       
+
+        
     }
 
     viewAllRoles(): void {
@@ -89,7 +245,7 @@ class Cli {
             } else if (result) {
                 console.log('\n');
                 printTable(result.rows);
-                const lineBuffer = Math.round(result.rowCount as number * 0.75);
+                const lineBuffer = Math.round(result.rowCount as number * 0.6);
                 for (let i = 0; i < lineBuffer; i++) {
                     console.log('\n');       
                 }
@@ -99,7 +255,62 @@ class Cli {
     }
 
     addRole(): void {
-        // TODO
+        // get array of available departments to add role to
+        const departments: string[] = [];
+        pool.query('SELECT name FROM department', (err: Error, result: QueryResult) => {
+            if (err) {
+              console.log(err);
+            } else if (result) {
+                result.rows.forEach(department => {
+                    departments.push(department.name);
+                });
+            }
+        });
+
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the role?',
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary of the role?',
+            },
+            {
+                type: 'list',
+                name: 'department',
+                message: 'Which department does the role belong to?',
+                choices: departments,
+            }
+        ])
+        .then((res) => {
+            // get department id by querying department.name
+            let departmentId;
+
+            pool.query(`SELECT id FROM department WHERE name = $1`, [res.department], (err: Error, result: QueryResult) => {
+                if (err) {
+                    console.log(err);
+                } else if (result) {
+                    // get the department id from the result object
+                    departmentId = result.rows[0].id;
+
+                    // insert the role into the correct department
+                    // perform this query inside of the first query due to async response causing the id to not be available otherwise
+                    pool.query(`INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)`, [res.name, res.salary, departmentId], (err: Error, result: QueryResult) => {
+                        if (err) {
+                        console.log(err);
+                        } else if (result) {
+                            console.log(`Added ${res.name} to the database`);
+                            this.startCli();
+                        }
+                    });
+        
+                }
+            });
+        });
     }
 
     viewAllDepartments(): void {
@@ -123,14 +334,14 @@ class Cli {
         inquirer
         .prompt([
             {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of the department?',
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the department?',
             }
         ])
         .then((res) => {
             // add the department
-            pool.query(`INSERT INTO department (name) VALUES ('${res.name}')`, (err: Error, result: QueryResult) => {
+            pool.query(`INSERT INTO department (name) VALUES ($1)`, [res.name], (err: Error, result: QueryResult) => {
                 if (err) {
                 console.log(err);
                 } else if (result) {
